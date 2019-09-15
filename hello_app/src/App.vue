fffb100ba1c31ef32c299a96627082b250acf706
TAKESHI MIYAJIMA <m71203@gmail.com>
2019-09-15 17:10:30 +0900

    p174

--- hello_app/src/App.vue
+++ hello_app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld title="Hello" message="This is Vue.js sample"/>
+    <HelloWorld title="Hello"/>
   </div>
 </template>
 
