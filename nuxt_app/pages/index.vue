dd0b3c350c88823fdfe55c5e29e17cf7477cb3e6
TAKESHI MIYAJIMA <m71203@gmail.com>
2019-09-17 19:38:24 +0900

    p319

--- nuxt_app/pages/index.vue
+++ nuxt_app/pages/index.vue
@@ -3,11 +3,8 @@
     <h1>{{title}}</h1>
     <p>{{$store.state.message}}</p>
     <hr>
-    <div class="link" @click="$store.commit({type:'count', message: 'add 1', add:1})">
-      <a>clicked(+1): {{ $store.state.counter }}</a>
-    </div>
-    <div class="link" @click="$store.commit({type:'count', message: 'add 2', add:2})">
-      <a>clicked(+2): {{ $store.state.counter }}</a>
+    <div class="link" @click="$store.dispatch('doit')">
+      <a>clicked: {{ $store.state.counter }}</a>
     </div>
     <div class="link" @click="$store.commit('reset')">
       <a>reset</a>
