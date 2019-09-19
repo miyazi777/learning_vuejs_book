9c1a1e2471a48f33caade7354d641e0bf7801fdf
TAKESHI MIYAJIMA <m71203@gmail.com>
2019-09-19 20:46:08 +0900

    p366

--- nuxt_app2/pages/axios.vue
+++ nuxt_app2/pages/axios.vue
@@ -2,6 +2,10 @@
   <section class="container">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
+    <div>
+      <input type="text" v-model="msg"/>
+      <button @click="doClick">click</button>
+    </div>
     <table>
       <tr>
         <th>User ID</th>
@@ -32,15 +36,22 @@ export default {
   data: function() {
     return {
       title: 'Axios',
-      msg: '',
       message: 'axios sample',
+      msg: '',
+      json_data: {},
     };
   },
-  asyncData: async function() {
-    let id = 1;
-    let result = await axios.get(url + id);
-    return { json_data: result.data }
-  },
+  methods: {
+    doClick: function(event) {
+      axios.get(url + this.msg).then((res) => {
+        this.message = 'get ID=' + this.msg;
+        this.json_data = res.data;
+      }).catch((error) => {
+        this.message = 'ERROR';
+        this.json_data = {};
+      });
+    }
+  }
 }
 </script>
 
