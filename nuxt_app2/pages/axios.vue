7f9042e36b8cea3ace12651a7c6dcd0c86154634
TAKESHI MIYAJIMA <m71203@gmail.com>
2019-09-19 20:40:54 +0900

    p359

--- nuxt_app2/pages/axios.vue
+++ nuxt_app2/pages/axios.vue
@@ -2,51 +2,58 @@
   <section class="container">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
-    <pre>{{ html_data }}</pre>
+    <table>
+      <tr>
+        <th>User ID</th>
+        <td>{{json_data.userId}}</td>
+      </tr>
+      <tr>
+        <th>ID</th>
+        <td>{{json_data.id}}</td>
+      </tr>
+      <tr>
+        <th>Title</th>
+        <td>{{json_data.title}}</td>
+      </tr>
+      <tr>
+        <th>Body</th>
+        <td>{{json_data.body}}</td>
+      </tr>
+    </table>
   </section>
 </template>
 
 <script>
 const axios = require('axios');
 
-let url = "/README.md";
+let url = "https://jsonplaceholder.typicode.com/posts/";
 
 export default {
   data: function() {
     return {
       title: 'Axios',
+      msg: '',
       message: 'axios sample',
     };
   },
   asyncData: async function() {
-    let result = await axios.get(url);
-    return { html_data: result.data }
+    let id = 1;
+    let result = await axios.get(url + id);
+    return { json_data: result.data }
   },
 }
 </script>
 
 <style>
-.container {
+tr th {
+  width: 150px;
+  background-color: darkblue;
+  color: white;
+  font-size: 16pt;
+}
+tr td {
   padding: 5px 10px;
-}
-h1 {
-  font-size: 60pt;
-  color: #345980;
-}
-p {
-  padding-top: 5px;
-  font-size: 20pt;
-}
-div {
+  background-color: #eef;
   font-size: 14pt;
-}
-pre {
-  padding: 10px;
-  font-size: 18pt;
-  background-color: #efefef;
-  white-space: pre-wrap;
-}
-hr {
-  margin: 10px 0px;
 }
 </style>
